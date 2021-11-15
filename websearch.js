edf1c46799ec4b1f07adbf5cdd7a12a8b09a3ea3
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2021-11-14 16:27:34 -0800

    Update websearch.js

--- websearch.js
+++ websearch.js
@@ -1,7 +1,7 @@
 //websearch.js	
 	$( 'option[value="#"]' ).prop( 'selected', 'selected' ); //jQuery reselects default#		      
 function dosearch() {
-var s_term=document.web_search_engines;
+var s_term=document.web_search_form;
 var submitto = s_term.search_engines.options[s_term.search_engines.selectedIndex].value + escape(s_term.searchterms.value);
 window.location.href = submitto;
 return false;
