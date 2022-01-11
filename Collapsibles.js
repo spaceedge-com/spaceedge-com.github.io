176789d803026b1e42264c9bba9c0940497bbb4f
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2022-01-10 20:02:49 -0800

    Update Collapsibles.js

--- Collapsibles.js
+++ Collapsibles.js
@@ -16,7 +16,8 @@ for (i = 0; i < coll.length; i++) {
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.maxHeight = content.scrollHeight + "px";  
+    }
   });
 }
 //</script>
