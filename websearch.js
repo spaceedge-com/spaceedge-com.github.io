1fed2015559f93c89736d6fa59225e57aeb8923c
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2022-01-10 18:54:18 -0800

    Update websearch.js

--- websearch.js
+++ websearch.js
@@ -1,47 +1,23 @@
 //websearch.js
-	$( 'option[value="#"]' ).prop( 'selected', 'selected' );  //jQuery reselect #
+   $( 'option[value="#"]' ).prop( 'selected', 'selected' ); //jQuery reselect #
 	
 function dosearch() {
 
 var si = (web_search_form.search_engines.value);
 var term = (web_search_form.searchterms.value);
 
-if (si=="ask") {
-		var seng = "https://ask.com/web?q="; 
-} 
-if (si === "bing") {
-		var seng = "https://www.bing.com/search?q=";	
-} 
-if (si === "dogpile") {                                                         
-        var seng = "https://www.dogpile.com/info.dogpl/search/web?q=";   
-} 
-if (si === "duckduckg") {  
-	    var seng = "https://duckduckgo.com/?q=";                                     
-}
-if (si === "ecosia") {  
-	    var seng = "https://www.ecosia.org/search?q=";                          
-}
-if (si === "lilo") {  
-	    var seng = "https://search.lilo.org/searchweb.php?q=";                       
-}
-if (si === "qwant") {  
-	    var seng = "https://www.qwant.com/?q=";                            
-}
-if (si === "startpage") {  
-	    var seng = "https://www.startpage.com/do/search?query=";
-}
-if (si === "swisscows") {  
-	    var seng = "https://swisscows.ch/?query=";                                       
-}
-if (si === "wikipedia") {  
-        var seng = "https://en.wikipedia.org/wiki/wiki.html?search=";
-}
-if (si === "wolframalpha") {
-        var seng = "https://www.wolframalpha.com/input/?i=";
-}
-if (si === "yahoo") {
-	    var seng = "https://search.yahoo.com/search?p=";  
-} 
+if (si=="ask") {  var seng = "https://ask.com/web?q=";  } 
+if (si === "bing") {  var seng = "https://www.bing.com/search?q=";  } 
+if (si === "dogpile") {  var seng = "https://www.dogpile.com/info.dogpl/search/web?q=";  } 
+if (si === "duckduckg") {  var seng = "https://duckduckgo.com/?q=";  }
+if (si === "ecosia") {  var seng = "https://www.ecosia.org/search?q=";  }
+if (si === "lilo") {  var seng = "https://search.lilo.org/searchweb.php?q=";  }
+if (si === "qwant") {  var seng = "https://www.qwant.com/?q=";  }
+if (si === "startpage") {  var seng = "https://www.startpage.com/do/search?query=";  }
+if (si === "swisscows") {  var seng = "https://swisscows.ch/?query=";  }
+if (si === "wikipedia") {  var seng = "https://en.wikipedia.org/wiki/wiki.html?search=";  }
+if (si === "wolframalpha") {  var seng = "https://www.wolframalpha.com/input/?i=";  }
+if (si === "yahoo") {  var seng = "https://search.yahoo.com/search?p=";  } 
 
 var submit = seng + term;
 //alert ("After selection (submit): >>>> : " + submit);
