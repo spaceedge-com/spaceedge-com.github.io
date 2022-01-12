55ce1fa6e13cbcdd3bd880ab0bdc84b4067f0847
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2022-01-12 12:43:12 -0800

    Update WebSearch.js

--- WebSearch.js
+++ WebSearch.js
@@ -11,8 +11,12 @@ if (si === "bing") {  var seng = "https://www.bing.com/search?q=";  }
 if (si === "dogpile") {  var seng = "https://www.dogpile.com/info.dogpl/search/web?q=";  } 
 if (si === "duckduckg") {  var seng = "https://duckduckgo.com/?q=";  }
 if (si === "ecosia") {  var seng = "https://www.ecosia.org/search?q=";  }
+if (si === "gitHub") {  var seng = "https://github.com/search?q={searchTerms}&ref=opensearch";  }
 if (si === "lilo") {  var seng = "https://search.lilo.org/searchweb.php?q=";  }
+if (si === "onelook") {  var seng = "https://onelook.com/?w=";  }
 if (si === "qwant") {  var seng = "https://www.qwant.com/?q=";  }
+if (si === "stackexchange") {  var seng = "https://stackexchange.com/search?q=";  }
+if (si === "stackoverflow") {  var seng = "https://stackoverflow.com/search?q={searchTerms}";  }
 if (si === "startpage") {  var seng = "https://www.startpage.com/do/search?query=";  }
 if (si === "swisscows") {  var seng = "https://swisscows.ch/?query=";  }
 if (si === "wikipedia") {  var seng = "https://en.wikipedia.org/wiki/wiki.html?search=";  }
