bfef23651fbe64b3beb46d7cd83ef2e63139bd80
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2022-07-29 22:24:59 -0700

    Update rnd-backgrnd.js

--- rnd-backgrnd.js
+++ rnd-backgrnd.js
@@ -1,7 +1,7 @@
 if (document.getElementById) { window.onload = swap };
 function swap() {
-var numimages=10;
-rndimg = new Array("/images/slider/astronomy1.jpg", "/images/slider/astronomy2.jpg","/images/slider/astronomy3.jpg", "/images/slider/astronomy4.jpg", "/images/slider/astronomy5.jpg", "/images/slider/astronomy6.jpg", "/images/slider/astronomy7.jpg" ,"/images/slider/astronomy8.jpg", "/images/slider/astronomy9.jpg");
+var numimages=9;
+rndimg = new Array("/images/slider/astronomy1.jpg", "/images/slider/astronomy2.jpg", "/images/slider/astronomy3.jpg", "/images/slider/astronomy4.jpg", "/images/slider/astronomy5.jpg", "/images/slider/astronomy6.jpg", "/images/slider/astronomy7.jpg", "/images/slider/astronomy8.jpg", "/images/slider/astronomy9.jpg");
 x=(Math.floor(Math.random()*numimages));
 randomimage=(rndimg[x]);
 document.getElementById("body").style.backgroundImage = "url("+ randomimage +")";
