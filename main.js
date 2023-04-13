3662b30d9169b7445257e45e37319a05699d1b8a
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2023-04-13 15:46:14 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,5 +4,12 @@ window.onload = () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
              .register('./sw.js');
+              //
+              //clear sw.js cache
+              caches.keys().then(function(cacheNames) {
+              cacheNames.forEach(function(spaceedge) {
+              caches.delete(spaceedge);
+    });
+  });
   }
 }
