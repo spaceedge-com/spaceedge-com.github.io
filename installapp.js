a6f6fa4f3135dcf16c799a684466fc1384d8139b
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2023-04-13 21:16:04 -0700

    Update installapp.js

--- installapp.js
+++ installapp.js
@@ -2,11 +2,16 @@
 
 var beforeInstallPrompt = null;
 
+document.getElementById('installBtn').style.visibility= 'hidden';
+
 window.addEventListener("beforeinstallprompt", eventHandler, errorHandler);
 
 function eventHandler(event) {
   beforeInstallPrompt = event;
   document.getElementById("installBtn").removeAttribute("disabled");
+  
+  document.getElementById('btn').style.visibility= 'visible';
+  
 }
 
 function errorHandler(event) {
