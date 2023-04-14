156bc2fcd733fdb3a4663850f244e3f2dbbc2c11
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2023-04-13 19:41:36 -0700

    Update installapp.js

--- installapp.js
+++ installapp.js
@@ -1,9 +1,13 @@
 let deferredPrompt;
-    window.addEventListener('beforeinstallprompt', (e) => {
-        deferredPrompt = e;
-    });
 
-    const installApp = document.getElementById('installApp');
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 67 and earlier from automatically showing the prompt
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+});
+
+const installApp = document.getElementById('installApp');
     installApp.addEventListener('click', async () => {
         if (deferredPrompt !== null) {
             deferredPrompt.prompt();
@@ -12,4 +16,4 @@ let deferredPrompt;
                 deferredPrompt = null;
             }
         }
-    });
+});
