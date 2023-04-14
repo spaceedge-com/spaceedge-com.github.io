65922fbee7752c52b10bfa0b09fcd24cd46951ce
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2023-04-13 19:03:05 -0700

    Create installapp.js

---
+++ installapp.js
@@ -0,0 +1,15 @@
+let deferredPrompt;
+    window.addEventListener('beforeinstallprompt', (e) => {
+        deferredPrompt = e;
+    });
+
+    const installApp = document.getElementById('installApp');
+    installApp.addEventListener('click', async () => {
+        if (deferredPrompt !== null) {
+            deferredPrompt.prompt();
+            const { outcome } = await deferredPrompt.userChoice;
+            if (outcome === 'accepted') {
+                deferredPrompt = null;
+            }
+        }
+    });
