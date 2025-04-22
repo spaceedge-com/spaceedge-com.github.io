84a3c128f66958ab9cfa6272bc7e7e47a71a98f9
spaceedge <83145033+spaceedge-com@users.noreply.github.com>
2025-04-21 17:48:25 -0700

    Update service-worker.js

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,1 +1,9 @@
 
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installed');
+  // Cache essential files
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('Service Worker activated');
+});
